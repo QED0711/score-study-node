6861d8112afcb5a4279db7c4a8820d07b7a8a934
QED0711 <qdizon@gmail.com>
2019-11-10 06:08:18 -0500

    added password encryption for user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
 const mlab = require("../keys").mlab;
 const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require("mongodb").ObjectID
+const ObjectID = require("mongodb").ObjectID;
 const url = `mongodb://${mlab.username}:${mlab.password}@ds233268.mlab.com:33268/score-study-users`;
 const dbName = "score-study-users";
 const connectionSettings = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const bcrypt = require("bcrypt");
+const salt = 10;
 
 const usersController = {
   /* 
@@ -13,7 +16,10 @@ const usersController = {
     */
   createUser: (req, res) => {
     const data = req.body;
+
     data.authorization = data.authorization || "standard";
+    data.password = bcrypt.hashSync(data.password, salt);
+
     MongoClient.connect(url, connectionSettings, async (err, client) => {
       const userCollection = client.db(dbName).collection("users");
 
@@ -25,13 +31,13 @@ const usersController = {
         res.send({ error: "Username already exists" });
       } else {
         let newUser = await userCollection.insertOne(data);
-        newUser = newUser.ops[0]
-        console.log(newUser)
+        newUser = newUser.ops[0];
+        console.log(newUser);
         res.send({
-            username: newUser.username,
-            email: newUser.email,
-            authorization: newUser.authorization,
-            userID: newUser._id
+          username: newUser.username,
+          email: newUser.email,
+          authorization: newUser.authorization,
+          userID: newUser._id
         });
       }
     });
@@ -48,11 +54,13 @@ const usersController = {
       const userCollection = client.db(dbName).collection("users");
 
       const existingUser = await userCollection.findOne({
-        username: data.username,
-        password: data.password
+        username: data.username
       });
 
-      if (existingUser) {
+      if (
+        existingUser &&
+        bcrypt.compareSync(data.password, existingUser.password)
+      ) {
         res.send({
           username: existingUser.username,
           userID: existingUser._id,
@@ -81,19 +89,22 @@ const usersController = {
 
   changePassword: (req, res) => {
     const data = req.body;
+
     MongoClient.connect(url, connectionSettings, async (err, client) => {
       const userCollection = client.db(dbName).collection("users");
 
       const existingUser = await userCollection.findOne({
         _id: new ObjectID(data.userID),
-        username: data.username,
-        password: data.currentPassword
+        username: data.username
       });
 
-      if (existingUser) {
+      if (
+        existingUser &&
+        bcrypt.compareSync(data.currentPassword, existingUser.password)
+      ) {
         await userCollection.updateOne(
           { _id: existingUser._id },
-          { $set: { password: data.newPassword } }
+          { $set: { password: bcrypt.hashSync(data.newPassword, salt) } }
         );
         res.send(existingUser._id);
       } else {
@@ -101,25 +112,25 @@ const usersController = {
       }
     });
   },
-  
+
   /* 
   :::::::::::::::::::::::
   :: CHANGE USER EMAIL ::
   :::::::::::::::::::::::
   */
 
- changeEmail: (req, res) => {
+  changeEmail: (req, res) => {
     const data = req.body;
-    console.log(data)
+    console.log(data);
     MongoClient.connect(url, connectionSettings, async (err, client) => {
       const userCollection = client.db(dbName).collection("users");
 
       const existingUser = await userCollection.findOne({
-        _id: new ObjectID(data.userID),
+        _id: new ObjectID(data.userID)
         // username: data.username
       });
 
-      console.log(typeof existingUser._id)
+      console.log(typeof existingUser._id);
 
       if (existingUser) {
         await userCollection.updateOne(
@@ -127,16 +138,16 @@ const usersController = {
           { $set: { email: data.email } }
         );
         res.send({
-            username: existingUser.username,
-            email: data.email,
-            authorization: existingUser.authorization,
-            userID: existingUser._id
+          username: existingUser.username,
+          email: data.email,
+          authorization: existingUser.authorization,
+          userID: existingUser._id
         });
       } else {
         res.send({ error: "no user found with those credentials" });
       }
     });
-  },
+  }
 };
 
 module.exports = usersController;
